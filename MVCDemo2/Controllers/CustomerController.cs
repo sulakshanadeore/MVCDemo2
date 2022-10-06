e6cdfbe59e113dd689429722e50a5558327c5bc1
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2022-10-06 10:42:32 +0530

    Add project files.

---
+++ MVCDemo2/Controllers/CustomerController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo2.Models;
+namespace MVCDemo2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AcceptData()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AcceptData(CustModel model)
+        {
+            CustModel c = new CustModel();
+            c.Custid = model.Custid;
+            c.Custname = model.Custname;
+            c.City = model.City;
+            return View();
+        }
+
+
+        public ActionResult AddData()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddData(FormCollection customer)
+        {
+
+            CustModel c = new CustModel();
+
+            c.Custid = Convert.ToInt32(Request.Form["Custid"]);
+            c.Custname = customer["Custname"].ToString();
+            c.City = customer["City"].ToString();
+            return View();
+        }
+
+
+        public ActionResult AddNos()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddNos(int i,int j)
+        {
+            int ans = i + j;
+            return Content(ans.ToString());
+        }
+        public ActionResult CustData()
+        { 
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CustData(int id,string name, string city)
+        {
+            CustModel c = new CustModel();
+            c.Custid = id;
+            c.Custname = name;
+            c.City = city;
+            //passing the values using the route parameter
+            //route parameter is declared in Route.config(App_Start Folder)
+
+            return RedirectToAction("FindCust", new { custid =c.Custid,name=c.Custname});
+        }
+
+
+        public ActionResult FindCust(int custid,string cname)
+        {
+            string s = null;
+            string gotname = cname;
+            if (custid!=0)
+            {
+                s = "trying to find customer by its id";
+            }
+            return Content(s);
+        }
+
+
+    }
+}
---
+++ MVCDemo2/Models/CustModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo2.Models
+{
+    public class CustModel
+    {
+        [Required()]
+        public int Custid { get; set; }
+        [MaxLength(10,ErrorMessage ="Name cannot be greater than 10 characters")]
+        [MinLength(3,ErrorMessage ="Name cannot be less than 3 character")]
+        public string Custname { get; set; }
+        public string City { get; set; }
+    }
+}
